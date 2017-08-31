69651bfd96ea9c2fb1d98b909416ef61a425b817
xiaochang <qianchang.xiao@gmail.com>
2017-09-01 00:06:23 +0800

    msg

--- app/src/main/java/com/changxiao/utilscollectiondemo/MainActivity.java
+++ app/src/main/java/com/changxiao/utilscollectiondemo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.changxiao.utilscollectiondemo;
 
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.changxiao.utilscollectiondemo.utils.BitmapCut;
 import com.changxiao.utilscollectiondemo.utils.BitmapUtil;
 import com.changxiao.utilscollectiondemo.utils.DeviceInfo;
 import com.changxiao.utilscollectiondemo.utils.ImageTools;
@@ -17,7 +20,14 @@ public class MainActivity extends AppCompatActivity {
     ImageView imgBgOne;
     ImageView imgBgTwo;
     ImageView imgBgThree;
+    LinearLayout llContainer;
+    LinearLayout llContainer2;
     Bitmap bgBitmap;
+
+    String imgUrl = "http://imgsrc.baidu.com/image/c0%3Dshijue1%2C0%2C0%2C294%2C40/sign=3d2175" +
+            "db3cd3d539d530078052ee8325/b7003af33a87e950c1e1a6491a385343fbf2b425.jpg";
+//    String imgUrl = "http://imgsrc.baidu.com/image/c0%3Dshijue1%2C0%2C0%2C294%2C40/sign=" +
+//            "6be2aa3503f79052fb124f7d649abdbf/9922720e0cf3d7ca308de650f81fbe096b63a999.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +37,15 @@ public class MainActivity extends AppCompatActivity {
         imgBgOne = (ImageView) findViewById(R.id.img_bg_one);
         imgBgTwo = (ImageView) findViewById(R.id.img_bg_two);
         imgBgThree = (ImageView) findViewById(R.id.img_bg_three);
+        llContainer = (LinearLayout) findViewById(R.id.ll_container);
+        llContainer2 = (LinearLayout) findViewById(R.id.ll_container2);
 
         new AsyncTask<Voice, Voice, Bitmap>() {
             @Override
             protected Bitmap doInBackground(Voice... params) {
                 Bitmap image = null;
                 try {
-                    image = BitmapUtil.getImage("http://imgsrc.baidu.com/image/c0%3Dshijue1%2C0%2C0%2C294%2C40/sign=3d2175db3cd3d539d530078052ee8325/b7003af33a87e950c1e1a6491a385343fbf2b425.jpg");
+                    image = BitmapUtil.getImage(imgUrl);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -51,8 +63,30 @@ public class MainActivity extends AppCompatActivity {
                 imgBgTwo.setImageBitmap(bgBitmap);
 
                 // 该方法会压缩图片显示
-                Bitmap bitmapBySize = ImageTools.createBitmapBySize(bitmap, DeviceInfo.getDeviceWidth(), DeviceInfo.dp2px(150));
-                imgBgThree.setImageBitmap(bitmapBySize);
+//                Bitmap bitmapBySize = ImageTools.createBitmapBySize(bitmap, DeviceInfo.getDeviceWidth(), DeviceInfo.dp2px(150));
+//                imgBgThree.setImageBitmap(bitmapBySize);
+//                Bitmap cropBitmap = BitmapUtil.cropBitmap(bitmap, DeviceInfo.getDeviceWidth(), DeviceInfo.dp2px(150));
+//                imgBgThree.setImageBitmap(cropBitmap);
+
+                // 按照一定的宽高比例裁剪图片
+//                Bitmap cropBitmap = BitmapCut.ImageCrop(bitmap, DeviceInfo.getDeviceWidth(), DeviceInfo.dp2px(150), false);
+//                imgBgThree.setImageBitmap(cropBitmap);
+
+                // 按照一定的宽高比例裁剪图片
+                Bitmap cropBitmap = BitmapCut.ImageCrop(bitmap, DeviceInfo.getDeviceWidth(), DeviceInfo.dp2px(550));
+                imgBgThree.setImageBitmap(cropBitmap);
+
+                // 按照一定的宽高比例裁剪图片
+//                Bitmap bgBitmap = BitmapCut.ImageCrop(bitmap, llContainer.getWidth(), llContainer.getHeight());
+                llContainer.setBackground(new BitmapDrawable(bgBitmap));
+
+                // 按照一定的宽高裁剪图片（最终想要的效果）
+                Bitmap bgBitmap = BitmapCut.ImageCrop(bitmap, llContainer.getWidth(), llContainer.getHeight());
+                llContainer2.setBackground(new BitmapDrawable(bgBitmap));
+
+                // 按长方形裁切图片
+//                Bitmap cropBitmap = BitmapCut.ImageCrop(bitmap, false);
+//                imgBgThree.setImageBitmap(cropBitmap);
             }
         }.execute();
 
--- app/src/main/java/com/changxiao/utilscollectiondemo/utils/BitmapUtil.java
+++ app/src/main/java/com/changxiao/utilscollectiondemo/utils/BitmapUtil.java
@@ -171,6 +171,22 @@ public class BitmapUtil {
     }
 
     /**
+     * 裁剪
+     *
+     * @param bitmap 原图
+     * @return 裁剪后的图像
+     */
+    public static Bitmap cropBitmap(Bitmap bitmap, int targetWidth, int targetHight) {
+        int w = bitmap.getWidth(); // 得到图片的宽，高
+        int h = bitmap.getHeight();
+
+        int cropWidth = w >= h ? h : w;// 裁切后所取的正方形区域边长
+        cropWidth /= 2;
+        int cropHeight = (int) (cropWidth / 1.2);
+        return Bitmap.createBitmap(bitmap, w / 3, 0, cropWidth, cropHeight, null, false);
+    }
+
+    /**
      * 将Drawable转为Bitmap
      */
     public static Bitmap drawableToBitmap(Drawable drawable) {
